7a0a549c4f156291e27fa9a965c65d64c539c41f
Suhel Khan <suhelk3003btechiot2026@kccitm.edu.in>
2024-05-02 21:23:25 +0530

    change some styling

--- frontend v0.1/src/components/card.jsx
+++ frontend v0.1/src/components/card.jsx
@@ -6,24 +6,23 @@ import TextLimited from "./textLimited";
 export default function Card({ link, count }) {
 
     return (
-        <a href={link} target="_blank" rel="noreferrer" className="flex flex-col justify-center items-center px-3 rounded-xl border border-solid bg-neutral-700 border-neutral-500 h-[182px] w-[182px] hover:shadow-xl hover:shadow-[#020202] hover:duration-200 cursor-pointer hover:border-[#ffffff71]">
+        <a href={link} target="_blank" rel="noreferrer" className="flex flex-col justify-center items-center px-3 rounded-xl border-2 border-solid bg-[#424242] border-[#ffffff12] h-[182px] w-[182px] hover:shadow-xl hover:shadow-[#020202] duration-300 hover:scale-[103%] cursor-pointer hover:border-[#ffffff71] text-[#ffffffaf] hover:text-[#fcfcfcee] z-1">
             <div className="flex flex-col justify-center pb-7">
                 <img
                     src={linkImage}
                     alt=""
                     className=" mt-4 lg:ml-32 w-[20px] "
                 />
-                <div className="flex flex-col justify-center mt-1.5">
+                <div className="flex flex-col justify-center mt-1.5 ">
                     <div className="flex flex-col whitespace-nowrap">
                         {/* To get the log from the link User provide */}
-                        <img loading="lazy" src={`https://www.google.com/s2/favicons?domain=${link}&sz=128`} alt="" className="w-8 rounded-md" />
+                        <img loading="lazy" src={`https://www.google.com/s2/favicons?domain=${link}&sz=128`} alt="img" className="w-8 rounded-md hover:scale-110 duration-200" />
 
                         <div className="flex flex-col justify-center mt-1">
-                            <p className="mt-1.5 text-lg text-[#fcfcfcee]
-                            ">
+                            <p className="mt-1.5 text-lg ">
                                 <TextLimited text={extractDomain(link)} maxLength={16} />
                             </p>
-                            <p className=" text-[12px] text-[#ffffff] text-opacity-40 overflow-hidden">
+                            <p className=" text-[12px] text-[#ffffff] text-opacity-40 overflow-hidden ">
                                 <TextLimited text={link} maxLength={22} />
                             </p>
                         </div>
@@ -48,7 +47,7 @@ function extractDomain(url) {
         domain = domain.split('-')[0];
     }
     if (domain.includes('www.')) {
-        domain = domain.replace('www.', '');
+        domain = domain.replace('www.' || 'www4', '');
     }
 
     return domain;
